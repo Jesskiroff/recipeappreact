95c35b648d15418a75c30f8c86910258915f0fbd
Jessica Kiroff <jesskiroff@gmail.com>
2024-11-08 13:50:27 -0500

    add useEffect to Popular page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 function Popular() {
+    useEffect(() =>{
+    getPopular();
+  }, []);
 
     const getPopular = async () => {
         const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
