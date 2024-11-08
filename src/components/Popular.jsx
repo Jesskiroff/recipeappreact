3e49f9ec24ccc427c61fe045520957796b156b59
Jessica Kiroff <jesskiroff@gmail.com>
2024-11-08 13:45:38 -0500

    add to popular page

---
+++ src/components/Popular.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Popular() {
+
+    const getPopular = async () => {
+        const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+        const data = await api.json();
+        console.log(data);
+        //adding aaync here bc it's data that we n\eed 
+        //to wait for and we4 wanna make sure we have it before 
+        //
+    }
+
+
+  return (<div> </div>);
+}
+
+export default Popular
