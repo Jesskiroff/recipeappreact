8c9bc81d9099de8bedb804c39b3c589fab414c2c
Jessica Kiroff <jesskiroff@gmail.com>
2024-11-12 20:35:09 -0500

    add useEffect explaination

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,21 +1,23 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react';
 
 function Popular() {
-    useEffect(() =>{
+  useEffect(() => {
     getPopular();
   }, []);
+  // this is to run the function as soon as possible.The empty array essentially tells the comp to only run the func when the component gets mointed
 
-    const getPopular = async () => {
-        const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-        const data = await api.json();
-        console.log(data);
-        //adding aaync here bc it's data that we n\eed 
-        //to wait for and we4 wanna make sure we have it before 
-        //
-    }
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+    //adding aaync here bc it's data that we n\eed
+    //to wait for and we4 wanna make sure we have it before
+    //
+  };
 
-
-  return (<div> </div>);
+  return <div> </div>;
 }
 
-export default Popular
+export default Popular;
