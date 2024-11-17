380992535e5f48764a41d33ae5f8e4f6ecdde348
Jessica Kiroff <jesskiroff@gmail.com>
2024-11-17 18:50:46 -0500

    import splide and splide css

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components'; 
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
 
 function Popular() {
   const [popular, setPopular] = useState([]);
