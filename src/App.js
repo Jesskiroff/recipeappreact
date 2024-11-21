40f9b85387a85477ad0a1d851657d4e8de14f51c
Jessica Kiroff <jesskiroff@gmail.com>
2024-11-20 21:51:21 -0500

    add catgory icons and options

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Pages from "./pages/Pages";
+import Pages from './pages/Pages';
+import Category from './components/Category';
+
 function App() {
   return (
     <div className="App">
-    <Pages/>
+      <Category />
+      <Pages />
     </div>
   );
 }
