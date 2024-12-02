d8eab3db01ba018aae1c35876d6ac265fea90159
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-02 13:18:40 -0500

    add params

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -5,15 +5,20 @@ import { Link, useParams } from 'react-router-dom';
 
 
 function Cuisine() {
+    const[cusine, setCuisine] =useState([]);
+    let params = useParams ();
+
   const getCuisine = async (name) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+    const data = await fetch(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
     const recipes= await data.json();
     setCuisine(recipes.results);
   };
   
 
   useEffect(( )=> {
-    getCuisine
+    getCuisine(params.type);
     console.log(params.type);
   },[params.type]);
     return (
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -8,7 +8,7 @@ function Pages() {
       
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<cuisine />} />
+          <Route path="/" element={<Cuisine />} />
         </Routes>
       
     </div>
