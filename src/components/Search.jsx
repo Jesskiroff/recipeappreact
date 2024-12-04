372238e65210012bbfda0f1d7598a820ec844148
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-04 11:18:30 -0500

    initialize search file

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+
+function Search() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Search
