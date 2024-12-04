ba329f038a0ace909f83e061308926a4e8707d31
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-03 23:15:11 -0500

    add styled navlink

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,33 +1,34 @@
-import { MdOutlineFoodBank } from 'react-icons/md';
-import { GiMeal, GiCakeSlice, GiFrenchFries } from 'react-icons/gi';
+import { MdOutlineFastfood } from 'react-icons/md';
+import { GiMeal, GiHotSpices } from 'react-icons/gi';
+import { FaPepperHot, FaWineBottle } from 'react-icons/fa';
 import styled from 'styled-components';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 //NavLink givess you a class of active
 
 function Category() {
   return (
     <List>
-      <NavLink to={'/cuisine/American'}>
-        <MdOutlineFoodBank />
+      <SLink to={'/cuisine/American'}>
+        <MdOutlineFastfood />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/French'}>
+      </SLink>
+      <SLink to={'/cuisine/French'}>
         <GiMeal />
         <h4>French</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Jewish'}>
-        <GiFrenchFries />
+      </SLink>
+      <SLink to={'/cuisine/Jewish'}>
+        <FaWineBottle/>
         <h4>Jewish</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/LatinAmerican'}>
-        <GiFrenchFries />
+      </SLink>
+      <SLink to={'/cuisine/Latin American'}>
+        <FaPepperHot />
         <h4>Latin American</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/MiddleEastern'}>
-        <GiCakeSlice />
+      </SLink>
+      <SLink to={'/cuisine/Middle Eastern'}>
+        <GiHotSpices />
         <h4>Middle Eastern</h4>
-      </NavLink>
+      </SLink>
     </List>
   );
 }
@@ -37,4 +38,17 @@ const List = styled.div`
   justify-content: center;
   margin: 2rem 0 rem;
 `;
+const SLink = styled(NavLink)`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border-radius: 50%
+margin-right: 2rem;
+text-decoration:none;
+background: k=linear-gradient(35deg, #494949, #313131);
+width: 6rem;
+height: 6rem;
+`
 export default Category;
+
