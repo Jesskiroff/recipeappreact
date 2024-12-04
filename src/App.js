742d028276b9dc630d1235d6be9f36af7ad18a8b
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-04 15:02:17 -0500

    add search icon to search bar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Pages from './pages/Pages';
 import Cuisine from './pages/Cuisine';
 import Category from './components/Category';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Search from './components/Search';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Search/>
       <Category />
       <Routes>
         <Route path="/cuisine/:type" element={<Cuisine />} />
